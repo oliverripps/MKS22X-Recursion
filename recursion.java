de71099624f754222c36e63e7ef22be59b90245f
Oliver Ripps <oripps@stuy.edu>
2019-01-31 20:40:51 -0500

    added comments and removed commented out code

--- recursion.java
+++ recursion.java
@@ -1,58 +1,43 @@
 public class recursion{
-    /*You may write additional private methods */
-
-    /*Recursively find the sqrt using Newton's approximation
-     *tolerance is the allowed percent error the squared answer is away from n.
-     *precondition: n is non-negative
-
-
-    public static boolean isCloseEnough(double guess, double num, double tolerance){
-      if(num==0.0){
-        return (guess==0.0);
-      }
-      return ((Math.abs(guess*guess-num)/num) <= tolerance);
-    }*/
     public static double squareRoot(double num, double g, double t){
-      if(num==0){
+      if(num==0){//making sure 0 is accounted for because would throw error in % calculation
         return 0.0;
       }
-      if((Math.abs((num-g*g) / num) <= t)){
+      if((Math.abs((num-g*g) / num) <= t)){//checking if close enough
         return g;
       }
-      else{
+      else{//if not close enough, average the guess with the number/guess and rerun that guess
         double guess=(((num/g) + g)/2);
         return squareRoot(num,guess,t);
       }
     }
-    public static double sqrt(double n, double tolerance){
+    public static double sqrt(double n, double tolerance){//calls helper function
       return squareRoot(n,n/2,tolerance);
       }
 
-
-    /*Recursively find the n'th fibbonaci number in linear time
-     *fib(0) = 1; fib(1) = 1; fib(5) = 5
-     *precondition: n is non-negative
-     */
-    public static int fib(int n){
+    public static int fib(int n){//calls helper function
       return fib(n,0,1);
     }
 
     public static int fib(int n,int prev, int pprev){
-      if (n == 0){
+      if (n == 0){//first term is 0
         return 0;
       }
-      if (n == 1){
+      if (n == 1){//return fib value accumulated
         return pprev;
       }
 
-      return fib(n - 1, pprev, prev + pprev);
+      return fib(n - 1, pprev, prev + pprev);//recal recursivly
    }
 
 
 
-    /*As Per classwork*/
-    /*public static ArrayList<Integer> makeAllSums(){
-    }*/
+
+    public static ArrayList<Integer> makeAllSums(){
+
+
+
+    }
 
 
     public static void main(String[] args){
