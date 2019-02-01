0f88aed73aecfc34b215ac99dad2b7ad2d67a388
Oliver Ripps <oripps@stuy.edu>
2019-01-31 20:01:49 -0500

    commented out blank things causing compiling errors

--- recursion.java
+++ recursion.java
@@ -25,19 +25,21 @@ public class recursion{
     public static double sqrt(double n, double tolerance){
       return squareRoot(n,n/2,tolerance);
       }
-    }
+
 
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
      */
-    public static int fib(int n){
+    /*public static int fib(int n){
 
-    }
+    }*/
 
     /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(){
-    }
+    /*public static ArrayList<Integer> makeAllSums(){
+    }*/
+
+
     public static void main(String[] args){
       System.out.println(sqrt(64,.01));
 
