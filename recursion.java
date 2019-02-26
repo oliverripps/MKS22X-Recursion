79920881d611c309d2a0b393c4d7f63af7c6a2a1
Oliver Ripps <oripps@stuy.edu>
2019-02-25 20:50:15 -0500

    tested with K driver, working

--- recursion.java
+++ recursion.java
@@ -49,20 +49,80 @@ public class recursion{
   }
 
 
+  public static boolean closeEnough(double a, double b){
+    if(a==0.0 && b==0.0)return true;
+    if(a==0.0)return b < 0.00000000001;
+    if(b==0.0)return a < 0.00000000001;
+    return Math.abs(a-b)/a < 0.0001;//This is the normal % difference allowed
+
+}
+
+
+  public static void testFib(int testcase){
+    recursion r = new recursion();
+    int[] input = {0,1,2,3,5,30};
+    int[] output ={0,1,1,2,5,832040};
+    int max = input.length;
+    if(testcase < input.length){
+      int in = input[testcase];
+      try{
+
+        int ans = r.fib(in);
+        int correct = output[testcase];
+        if(ans == correct){
+          System.out.println("PASS test fib "+in+". "+correct);
+        }
+        else{
+          System.out.println("FAIL test fib"+in+". "+ans+" vs "+correct);
+
+        }
+      }catch(IllegalArgumentException n){
+        if(in < 0){
+          System.out.println("PASS test fib"+in+" IllegalArgumentException");
+        }else{
+          System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+        }
+      }catch(Exception e){
+        System.out.println(" FAIL Some exception in test case:"+in);
+      }
+    }
+  }
+
+
+  //testcase must be a valid index of your input/output array
+  public static void testSqrt(int testcase){
+    recursion r = new recursion();
+    double[] input = {0.0,1.0, 2.0, 4.0, 7.0};
+    double[] output = {0.0,1.0,1.4142135623730951,2.0,2.6457513110645907};
+    int max = input.length;
+    if(testcase < input.length){
+      double in = input[testcase];
+      try{
+        double ans = r.sqrt(in,.00001);
+        double correct = Math.sqrt(in);
+        if(closeEnough(ans,correct)){
+          System.out.println("PASS test sqrt "+in+" "+ans);
+        }
+        else{
+          System.out.println("FAIL test sqrt "+in+" "+ans+" vs "+correct);
+
+        }
+      }catch(IllegalArgumentException n){
+        if(in < 0){
+          System.out.println("PASS test sqrt"+in+" IllegalArgumentException");
+        }else{
+          System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+        }
+      }catch(Exception e){
+        System.out.println(" FAIL Some exception in test case:"+in);
+      }
+    }
+  }
 
 
     public static void main(String[] args){ //testing each function
-      System.out.println("Testing square root...");
-      System.out.println("Should print around 8: " + sqrt(64,.01));
-      System.out.println("Should print around 9: " + sqrt(81,.01));
-      System.out.println("Should print around 26: " + sqrt(676,.01));
-      System.out.println("Testing fibbonaci...");
-      System.out.println("Should print 0: " + fib(0));
-      System.out.println("Should print 5: " + fib(5));
-      System.out.println("Should print 55: " + fib(10));
-      System.out.println("Should print [1, 0]: " + makeAllSums(1));
-      System.out.println("Should print [3, 2, 1, 0]: " + makeAllSums(2));
-      System.out.println("Should print [15, 14, 13, 12, 12, 11, 10, 9, 11, 10, 9, 8, 8, 7, 6, 5, 10, 9, 8, 7, 7, 6, 5, 4, 6, 5, 4, 3, 3, 2, 1, 0]: " + makeAllSums(5));
-    }
+      testSqrt(3);
+      //testFib(5);
 
+}
 }
